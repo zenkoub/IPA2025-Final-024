6e20ad89909b3119a81f18dbf3ce4478e5ac6d30
zenkoub <koobun1234@gmail.com>
2025-10-24 14:50:44 +0700

    update: netconf_final.py changed functions and added router_ip var

--- netconf_final.py
+++ netconf_final.py
@@ -1,19 +1,21 @@
 from ncclient import manager
 import xmltodict
 
-m = manager.connect(
-    host=router_ip,
-    port=830,
-    username="admin",
-    password="cisco",
-    hostkey_verify=False
+def connect(router_ip):
+    return manager.connect(
+        host=router_ip,
+        port=830,
+        username="admin",
+        password="cisco",
+        hostkey_verify=False
     )
 
-def netconf_edit_config(netconf_config):
+def netconf_edit_config(m, netconf_config):
     return m.edit_config(target="running", config=netconf_config)
 
-def check_interface(studentID):
+def check_interface(studentID, router_ip):
     loopback_name = f"Loopback{studentID}"
+    m = connect(router_ip)
     netconf_filter = f"""
     <filter>
       <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
@@ -26,17 +28,18 @@ def check_interface(studentID):
     try:
         netconf_reply = m.get(netconf_filter)
         reply_dict = xmltodict.parse(netconf_reply.xml)
-        # Check if data is present in the reply
         if "data" in reply_dict and "interfaces" in reply_dict["data"]:
             return 200
         else:
             return 404
     except:
         return 404
+    finally:
+        m.close_session()
 
-def create(studentID):
+def create(studentID, router_ip):
     loopback_name = f"Loopback{studentID}"
-    if check_interface(studentID) == 200:
+    if check_interface(studentID, router_ip) == 200:
         return f"Cannot create: Interface {loopback_name}"
 
     last3 = int(str(studentID)[-3:])
@@ -66,20 +69,21 @@ def create(studentID):
       </interfaces>
     </config>
     """
-
+    m = connect(router_ip)
     try:
-        reply = netconf_edit_config(netconf_config)
+        reply = netconf_edit_config(m, netconf_config)
         if "<ok/>" in reply.xml:
             return f"Interface {loopback_name} is created successfully using Netconf"
         else:
             return f"Cannot create: Interface {loopback_name}"
     except:
         return f"Cannot create: Interface {loopback_name}"
+    finally:
+        m.close_session()
 
-
-def delete(studentID):
+def delete(studentID, router_ip):
     loopback_name = f"Loopback{studentID}"
-    if check_interface(studentID) != 200:
+    if check_interface(studentID, router_ip) != 200:
         return f"Cannot delete: Interface {loopback_name}"
 
     netconf_config = f"""
@@ -91,19 +95,21 @@ def delete(studentID):
       </interfaces>
     </config>
     """
-
+    m = connect(router_ip)
     try:
-        reply = netconf_edit_config(netconf_config)
+        reply = netconf_edit_config(m, netconf_config)
         if "<ok/>" in reply.xml:
             return f"Interface {loopback_name} is deleted successfully using Netconf"
         else:
             return f"Cannot delete: Interface {loopback_name}"
     except:
         return f"Cannot delete: Interface {loopback_name}"
+    finally:
+        m.close_session()
 
-def enable(studentID):
+def enable(studentID, router_ip):
     loopback_name = f"Loopback{studentID}"
-    if check_interface(studentID) != 200:
+    if check_interface(studentID, router_ip) != 200:
         return f"Cannot enable: Interface {loopback_name}"
 
     netconf_config = f"""
@@ -116,20 +122,21 @@ def enable(studentID):
       </interfaces>
     </config>
     """
-
+    m = connect(router_ip)
     try:
-        reply = netconf_edit_config(netconf_config)
+        reply = netconf_edit_config(m, netconf_config)
         if "<ok/>" in reply.xml:
             return f"Interface {loopback_name} is enabled successfully using Netconf"
         else:
             return f"Cannot enable: Interface {loopback_name}"
     except:
         return f"Cannot enable: Interface {loopback_name}"
+    finally:
+        m.close_session()
 
-
-def disable(studentID):
+def disable(studentID, router_ip):
     loopback_name = f"Loopback{studentID}"
-    if check_interface(studentID) != 200:
+    if check_interface(studentID, router_ip) != 200:
         return f"Cannot shutdown: Interface {loopback_name} (checked by Netconf)"
 
     netconf_config = f"""
@@ -142,18 +149,19 @@ def disable(studentID):
       </interfaces>
     </config>
     """
-
+    m = connect(router_ip)
     try:
-        reply = netconf_edit_config(netconf_config)
+        reply = netconf_edit_config(m, netconf_config)
         if "<ok/>" in reply.xml:
             return f"Interface {loopback_name} is shutdowned successfully using Netconf"
         else:
             return f"Cannot shutdown: Interface {loopback_name} (checked by Netconf)"
     except:
         return f"Cannot shutdown: Interface {loopback_name} (checked by Netconf)"
+    finally:
+        m.close_session()
 
-
-def status(studentID):
+def status(studentID, router_ip):
     loopback_name = f"Loopback{studentID}"
     netconf_filter = f"""
     <filter>
@@ -164,7 +172,7 @@ def status(studentID):
       </interfaces-state>
     </filter>
     """
-
+    m = connect(router_ip)
     try:
         reply = m.get(netconf_filter)
         reply_dict = xmltodict.parse(reply.xml)
@@ -174,7 +182,6 @@ def status(studentID):
             intf_data = reply_dict["data"]["interfaces-state"]["interface"]
             admin_status = intf_data.get("admin-status")
             oper_status = intf_data.get("oper-status")
-
             if admin_status == "up" and oper_status == "up":
                 return f"Interface {loopback_name} is enabled (checked by Netconf)"
             elif admin_status == "down" and oper_status == "down":
@@ -183,3 +190,5 @@ def status(studentID):
             return f"No Interface {loopback_name} (checked by Netconf)"
     except:
         return f"No Interface {loopback_name} (checked by Netconf)"
+    finally:
+        m.close_session()
