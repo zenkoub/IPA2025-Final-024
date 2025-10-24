63c4ba5494a8d905359709f4a9452087ae5b0ac4
zenkoub <koobun1234@gmail.com>
2025-10-24 15:51:44 +0700

    update: netmiko.py, implement router_ip var

--- netmiko_final.py
+++ netmiko_final.py
@@ -5,29 +5,29 @@ from paramiko.transport import Transport
 Transport._preferred_kex = ('diffie-hellman-group14-sha1',)
 Transport._preferred_keys = ('ssh-rsa',)
 
-device_ip = "10.0.15.64"
-username = "admin"
-password = "cisco"
+def connect_router(router_ip):
+    device_params = {
+        "device_type": "cisco_ios",
+        "ip": router_ip,
+        "username": "admin",
+        "password": "cisco",
+        "ssh_config_file": False,
+        "allow_agent": False,
+        "conn_timeout": 30,
+        "global_delay_factor": 2,
+    }
+    return ConnectHandler(**device_params, session_log="netmiko_debug.txt")
 
-device_params = {
-    "device_type": "cisco_ios",
-    "ip": device_ip,
-    "username": username,
-    "password": password,
-    "ssh_config_file": False,
-    "allow_agent": False,
-    "conn_timeout": 30,
-    "global_delay_factor": 2,
-}
-
-def gigabit_status():
+def gigabit_status(router_ip):
     ans = ""
-    with ConnectHandler(**device_params, session_log="netmiko_debug.txt") as ssh:
+    with connect_router(router_ip) as ssh:
         ssh.send_command("terminal length 0")
         up = 0
         down = 0
         admin_down = 0
+        
         result = ssh.send_command("show ip interface brief", use_textfsm=True)
+        
         interface_status_list = []
         for status in result:
             if status["interface"].startswith("GigabitEthernet"):
@@ -38,6 +38,7 @@ def gigabit_status():
                     down += 1
                 elif status["status"] == "administratively down":
                     admin_down += 1
+        
         ans = (
             ", ".join(interface_status_list)
             + f" -> {up} up, {down} down, {admin_down} administratively down"
