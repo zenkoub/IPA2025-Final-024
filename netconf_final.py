64f423849a674c39119f43b619ad5e884fbb112c
zenkoub <koobun1234@gmail.com>
2025-10-24 18:00:17 +0700

    update: netconf success

--- netconf_final.py
+++ netconf_final.py
@@ -1,5 +1,6 @@
 from ncclient import manager
 import xmltodict
+import time
 
 def connect(router_ip):
     return manager.connect(
@@ -9,7 +10,6 @@ def connect(router_ip):
         password="cisco",
         hostkey_verify=False
     )
-
 
 def netconf_edit_config(m, netconf_config):
     return m.edit_config(target="running", config=netconf_config)
@@ -28,22 +28,22 @@ def check_interface(studentID, router_ip):
     </filter>
     """
     try:
-        netconf_reply = m.get(netconf_filter)
+        netconf_reply = m.get_config(source="running", filter=netconf_filter)
         reply_dict = xmltodict.parse(netconf_reply.xml)
-        if "data" in reply_dict and "interfaces" in reply_dict["data"]:
+        interfaces = reply_dict.get("rpc-reply", {}).get("data", {}).get("interfaces", {}).get("interface")
+        if interfaces:
             return 200
-        else:
-            return 404
+        return 404
     except:
         return 404
     finally:
         m.close_session()
 
 
-def create(studentID, router_ip):
+def create(studentID, router_ip, enabled=True):
     loopback_name = f"Loopback{studentID}"
     if check_interface(studentID, router_ip) == 200:
-        return f"Cannot create: Interface {loopback_name}"
+        return f"Cannot create: Interface {loopback_name} already exists"
 
     last3 = int(str(studentID)[-3:])
     x = last3 // 100
@@ -51,7 +51,7 @@ def create(studentID, router_ip):
     if y == 0:
         y = 1
     ip_addr = f"172.{x}.{y}.1"
-    
+
     netconf_config = f"""
     <config>
       <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
@@ -61,7 +61,7 @@ def create(studentID, router_ip):
           <type xmlns:ianaift="urn:ietf:params:xml:ns:yang:iana-if-type">
             ianaift:softwareLoopback
           </type>
-          <enabled>true</enabled>
+          <enabled>{str(enabled).lower()}</enabled>
           <ipv4 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip">
             <address>
               <ip>{ip_addr}</ip>
@@ -76,7 +76,8 @@ def create(studentID, router_ip):
     try:
         reply = netconf_edit_config(m, netconf_config)
         if "<ok/>" in reply.xml:
-            return f"Interface {loopback_name} is created successfully using Netconf"
+            status_str = "enabled" if enabled else "disabled"
+            return f"Interface {loopback_name} is created successfully ({status_str}) using Netconf"
         else:
             return f"Cannot create: Interface {loopback_name}"
     except:
@@ -111,48 +112,30 @@ def delete(studentID, router_ip):
     finally:
         m.close_session()
 
+
 def enable(studentID, router_ip):
     loopback_name = f"Loopback{studentID}"
     if check_interface(studentID, router_ip) != 200:
-        return f"Cannot enable: Interface {loopback_name}"
-
-    last3 = int(str(studentID)[-3:])
-    x = last3 // 100
-    y = last3 % 100
-    if y == 0:
-        y = 1
-    ip_addr = f"172.{x}.{y}.1"
+        return f"Cannot enable: Interface {loopback_name} does not exist"
 
     netconf_config = f"""
     <config>
       <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
         <interface>
           <name>{loopback_name}</name>
-          <description>Interface for student {studentID}</description>
-          <type xmlns:ianaift="urn:ietf:params:xml:ns:yang:iana-if-type">
-            ianaift:softwareLoopback
-          </type>
           <enabled>true</enabled>
-          <ipv4 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip">
-            <address>
-              <ip>{ip_addr}</ip>
-              <netmask>255.255.255.0</netmask>
-            </address>
-          </ipv4>
         </interface>
       </interfaces>
     </config>
     """
-
     m = connect(router_ip)
     try:
         reply = netconf_edit_config(m, netconf_config)
         if "<ok/>" in reply.xml:
-            return f"Interface {loopback_name} is enabled successfully using Netconf"
-        else:
-            return f"Cannot enable: Interface {loopback_name}"
-    except Exception as e:
-        return f"Cannot enable: Interface {loopback_name} ({str(e)})"
+            return f"Interface {loopback_name} is enabled using Netconf"
+        return f"Cannot enable: Interface {loopback_name}"
+    except:
+        return f"Cannot enable: Interface {loopback_name}"
     finally:
         m.close_session()
 
@@ -160,7 +143,7 @@ def enable(studentID, router_ip):
 def disable(studentID, router_ip):
     loopback_name = f"Loopback{studentID}"
     if check_interface(studentID, router_ip) != 200:
-        return f"Cannot shutdown: Interface {loopback_name} (checked by Netconf)"
+        return f"Cannot disable: Interface {loopback_name} does not exist"
 
     netconf_config = f"""
     <config>
@@ -176,42 +159,40 @@ def disable(studentID, router_ip):
     try:
         reply = netconf_edit_config(m, netconf_config)
         if "<ok/>" in reply.xml:
-            return f"Interface {loopback_name} is shutdowned successfully using Netconf"
-        else:
-            return f"Cannot shutdown: Interface {loopback_name} (checked by Netconf)"
+            return f"Interface {loopback_name} is disabled using Netconf"
+        return f"Cannot disable: Interface {loopback_name}"
     except:
-        return f"Cannot shutdown: Interface {loopback_name} (checked by Netconf)"
+        return f"Cannot disable: Interface {loopback_name}"
     finally:
         m.close_session()
 
 
 def status(studentID, router_ip):
     loopback_name = f"Loopback{studentID}"
+    time.sleep(1)
     netconf_filter = f"""
     <filter>
-      <interfaces-state xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+      <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
         <interface>
           <name>{loopback_name}</name>
         </interface>
-      </interfaces-state>
+      </interfaces>
     </filter>
     """
     m = connect(router_ip)
     try:
-        reply = m.get(netconf_filter)
+        reply = m.get_config(source="running", filter=netconf_filter)
         reply_dict = xmltodict.parse(reply.xml)
-        if ("data" in reply_dict and
-            "interfaces-state" in reply_dict["data"] and
-            "interface" in reply_dict["data"]["interfaces-state"]):
-            intf_data = reply_dict["data"]["interfaces-state"]["interface"]
-            admin_status = intf_data.get("admin-status")
-            oper_status = intf_data.get("oper-status")
-            if admin_status == "up" and oper_status == "up":
-                return f"Interface {loopback_name} is enabled (checked by Netconf)"
-            elif admin_status == "down" and oper_status == "down":
-                return f"Interface {loopback_name} is disabled (checked by Netconf)"
-        else:
+        interfaces = reply_dict.get("rpc-reply", {}).get("data", {}).get("interfaces", {}).get("interface")
+        if not interfaces:
             return f"No Interface {loopback_name} (checked by Netconf)"
+        if isinstance(interfaces, list):
+            interfaces = interfaces[0]
+        enabled = interfaces.get("enabled")
+        if enabled == "true":
+            return f"Interface {loopback_name} is enabled (checked by Netconf)"
+        else:
+            return f"Interface {loopback_name} is disabled (checked by Netconf)"
     except:
         return f"No Interface {loopback_name} (checked by Netconf)"
     finally:
